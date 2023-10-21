ec3cb95afef132df86d8a757104b0b1c9f131523
raocampo <rao.ocampo@yahoo.com>
2023-10-21 18:58:47 -0500

    Se adjunto el archivo avisos.js para crear las funciones en donde se ejecutaran los avisos para el usuario a travez de una venta modal, con sweetalert2

--- vistas/js/avisos.js
+++ vistas/js/avisos.js
@@ -1,1 +1,62 @@
-alert("Hola Mundo");
+const formularioAjax = document.querySelectorAll(".formularioAjax");
+
+function envioFormularioAjax(e){
+    
+    e.preventDefault();
+
+}
+
+formularioAjax.forEach(formularios => {
+    formularios.addEventListener("submit", envioFormularioAjax);
+});
+
+function avisoAjax(aviso){
+
+    if(aviso.Alerta === "simple"){
+        
+        Swal.fire({
+            title: aviso.Titulo,
+            text: aviso.Texto,
+            type: aviso.Tipo,
+            confirmButtonText: 'Aceptar'
+        });
+
+    }else if(aviso.Alerta === "recargar"){
+
+        Swal.fire({
+            title: aviso.Titulo,
+            text: aviso.Texto,
+            type: aviso.Tipo,
+            confirmButtonText: 'Aceptar'
+        }).then((result) => {
+            if(result.value){
+
+                location.reload();
+
+            }    
+        });
+
+    }else if(aviso.Alerta === "limpiar"){
+
+        Swal.fire({
+            title: aviso.Titulo,
+            text: aviso.Texto,
+            type: aviso.Tipo,
+            confirmButtonText: 'Aceptar'
+        }).then((result) => {
+            if(result.value){
+
+                document.querySelector(".formularioAjax").reset();
+
+            }    
+        });
+
+
+    }else if(aviso.Alerta === "redireccionar"){
+        
+        window.location.href=aviso.URL;
+
+    }
+
+
+}
